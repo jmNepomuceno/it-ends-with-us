34c47961c9e5797777e3b128311fcc0d908a027d
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2022-02-03 23:53:56 +0800

    02-03-22_b

--- book_page/book.js
+++ book_page/book.js
@@ -1,3 +1,8 @@
+// MAIN BOOK
+const book_section = document.querySelector('.book-section')
+
+// BOOK PAGES 
+const pages_div = document.querySelector('.pages-div')
 const prev_btn = document.querySelector(".prev-btn")
 const next_btn = document.querySelector(".next-btn")
 const book = document.querySelector(".pages-book-div")
@@ -10,6 +15,10 @@ let currentLocation = 1;
 let numOfPapers = 3;
 let max_location = numOfPapers + 1;
 
+// 
+const open_book_btn = document.querySelector('.open-book-btn')
+let first_open_book = true
+
 const openBook = () =>{
     book.style.transform = "translateX(50%)";
     prev_btn.style.transform = "translateX(-180px)"
@@ -19,38 +28,26 @@ const openBook = () =>{
 const closeBook = (isAtBeginning) =>{
     if(isAtBeginning){
         book.style.transform = "translateX(0%)";
+        pages_div.style.visibility = "hidden"
+        book_section.style.display = "flex"
+        first_open_book = true
     }else{
         book.style.transform = "translateX(100%)";
-
     }
     prev_btn.style.transform = "translateX(0px)"
     next_btn.style.transform = "translateX(0px)"
+
+
 }
 
-prev_btn.addEventListener('click', ()=>{
-    if(currentLocation > 1){
-        switch(currentLocation){
-            case 2: 
-                closeBook(true);
-                page_1.classList.remove("flipped")
-                page_1.style.zIndex = 3;
-                break;
-            case 3: 
-                page_2.classList.remove("flipped")
-                page_2.style.zIndex = 2;
-                break;
-            case 4: 
-                openBook()
-                page_3.classList.remove("flipped")
-                page_3.style.zIndex = 1;
-                break;
-        }
-        currentLocation--;
+const goNextPage = (first) =>{
+    if(first){
+        pages_div.style.visibility = "visible"
+        book_section.style.display = "none"
+        first_open_book = false
     }
-})
-
-next_btn.addEventListener('click', ()=>{
-
+    
+    
     if(currentLocation < max_location){
         switch(currentLocation){
             case 1:
@@ -70,4 +67,33 @@ next_btn.addEventListener('click', ()=>{
         }
         currentLocation++;
     }
-})
+}
+
+const goPrevBtn = () => {
+    if(currentLocation > 1){
+        switch(currentLocation){
+            case 2: 
+                closeBook(true);
+                page_1.classList.remove("flipped")
+                page_1.style.zIndex = 3;
+                break;
+            case 3: 
+                page_2.classList.remove("flipped")
+                page_2.style.zIndex = 2;
+                break;
+            case 4: 
+                openBook()
+                page_3.classList.remove("flipped")
+                page_3.style.zIndex = 1;
+                break;
+        }
+        currentLocation--;
+    }
+}
+
+prev_btn.addEventListener('click', goPrevBtn)
+
+next_btn.addEventListener('click', goNextPage)
+
+// open book btn
+open_book_btn.addEventListener('click', ()=> goNextPage(first_open_book))
